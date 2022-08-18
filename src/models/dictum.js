db11ea457f2f3305652e24a61c5088404aa82202
Beau Bouchard <127320+BeauBouchard@users.noreply.github.com>
2022-08-18 18:27:37 -0400

    Update dictum.js

--- src/models/dictum.js
+++ src/models/dictum.js
@@ -106,12 +106,9 @@ function getStockInfo(req) {
             ? `📈 ${stock.rsi} Oversold ✅`
             : `${stock.rsi}`;
         console.log(util.inspect(stock));
-        stock.sma20a =
-          stock.sma20 > 0 ? `${stock.sma20} 🚩` : `${stock.sma20} ✅`;
-        stock.sma50a =
-          stock.sma50 > 0 ? `${stock.sma50} 🚩` : `${stock.sma50} ✅`;
-        stock.sma200a =
-          stock.sma200 > 0 ? `${stock.sma200} 🚩` : `${stock.sma200} ✅`;
+				stock.sma20a = (stock.sma20 - stock.price)>0 ? `${stock.sma20} 🚩` : `${stock.sma20} ✅`;
+				stock.sma50a = (stock.sma50 - stock.price)>0 ? `${stock.sma50} 🚩` : `${stock.sma50} ✅`;
+				stock.sma200a = (stock.sma200 - stock.price)>0 ? `${stock.sma200} 🚩` : `${stock.sma200} ✅`;
         const returnstring = `> *${stock.ticker}* - *${stock.name}*
 > *Current Price:* ${stock.price}	${stock.change}
 > *Relative Strength Index (RSI):* ${stock.rsi}
